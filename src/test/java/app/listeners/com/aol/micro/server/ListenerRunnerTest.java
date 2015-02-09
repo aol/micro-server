2b401bd58932620b2fad98d06106824eaddb7d9b
John McClean <john.mcclean@teamaol.com>
2015-02-09 23:03:45 +0000

    tests for servlet context listeners

---
+++ src/test/java/app/listeners/com/aol/micro/server/ListenerRunnerTest.java
@@ -0,0 +1,56 @@
+package app.listeners.com.aol.micro.server;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import javax.servlet.ServletContextListener;
+
+import nonautoscan.com.aol.micro.server.ConfiguredListener;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.aol.micro.server.MicroServerStartup;
+import com.aol.micro.server.module.ConfigurableModule;
+import com.aol.micro.server.testing.RestAgent;
+import com.google.common.collect.ImmutableList;
+
+@Configuration
+@ComponentScan(basePackages = { "app.listeners.com.aol.micro.server" })
+public class ListenerRunnerTest {
+
+	RestAgent rest = new RestAgent();
+	
+	MicroServerStartup server;
+	@Before
+	public void startServer(){
+		List<ServletContextListener> listeners = ImmutableList.of(new ConfiguredListener());
+		server = new MicroServerStartup( ListenerRunnerTest.class, ConfigurableModule.builder().context("listener-app").listeners(listeners ).build());
+		server.start();
+
+	}
+	
+	@After
+	public void stopServer(){
+		server.stop();
+	}
+	
+	@Test
+	public void testListeners() throws InterruptedException, ExecutionException{
+		
+		assertThat(AutodiscoveredListener.getCalled(),is(1));
+		assertThat(ConfiguredListener.getCalled(),is(1));
+	}
+	
+	
+
+	
+	
+}
