2a28b40a61f16beed36b4f8330864fb04890328a
Ke Wang <ke.wang@teamaol.com>
2015-03-10 13:48:41 +0000

    Spring @Configuration and Injecting Bean Dependencies as Method Parameters

--- micro-core/src/main/java/com/aol/micro/server/rest/resources/ConfigureResources.java
+++ micro-core/src/main/java/com/aol/micro/server/rest/resources/ConfigureResources.java
@@ -3,25 +3,26 @@ package com.aol.micro.server.rest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.aol.micro.server.events.JobsBeingExecuted;
 import com.aol.micro.server.events.RequestsBeingExecuted;
 
+@Configuration
 public class ConfigureResources {
-	
+
 	@Autowired
 	private List<RequestsBeingExecuted> activeQueries;
-	@Autowired
-	private JobsBeingExecuted activeJobs;
-	
+
 	@Bean
-	public ActiveResource activeResource(){
-		return new ActiveResource(activeQueries,activeJobs);
+	public ActiveResource activeResource(@Qualifier("jobsBeingExecuted") JobsBeingExecuted jobsBeingExecuted) {
+		return new ActiveResource(activeQueries, jobsBeingExecuted);
 	}
-	
+
 	@Bean
-	public ManifestResource manifest(){
+	public ManifestResource manifest() {
 		return new ManifestResource();
 	}
 }
