a0d4cb0fcd9498ca5cb0dc6bc1a1936052c6d84c
John McClean <john.mcclean@teamaol.com>
2015-03-19 16:15:41 +0000

    log servlets, filters, listeners

--- micro-core/src/main/java/com/aol/micro/server/servers/grizzly/ServletContextListenerConfigurer.java
+++ micro-core/src/main/java/com/aol/micro/server/servers/grizzly/ServletContextListenerConfigurer.java
@@ -7,15 +7,18 @@ import javax.servlet.ServletContextListener;
 import lombok.AllArgsConstructor;
 
 import org.glassfish.grizzly.servlet.WebappContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ContextLoaderListener;
 
-
 import com.aol.micro.server.rest.jersey.JerseySpringIntegrationContextListener;
+import com.aol.micro.server.servers.model.FilterData;
 import com.aol.micro.server.servers.model.ServerData;
 
 @AllArgsConstructor
 public class ServletContextListenerConfigurer {
 	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final ServerData serverData;
 	private final List<ServletContextListener> listenerData;
 	
@@ -24,8 +27,14 @@ public class ServletContextListenerConfigurer {
 		serverData.getRootContext()
 				.getBeansOfType(ServletContextListener.class)
 				.values()
+				.stream()
+				.peek(this::logListener)
 				.forEach(listener -> webappContext.addListener(listener));
 		listenerData.forEach(it -> webappContext.addListener(it));
+	}
+	private void logListener(ServletContextListener listener) {
+		logger.info("Registering servlet context listener {}",listener.getClass().getName());
+		
 	}
 	
 	
