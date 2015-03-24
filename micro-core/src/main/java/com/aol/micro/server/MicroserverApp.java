75d2e504e3aa52ae369fcf490422a4cbf49744f2
Castigliego, Marco <marco.castigliego@teamaol.com>
2015-03-24 12:12:51 +0000

    test

--- micro-core/src/main/java/com/aol/micro/server/MicroserverApp.java
+++ micro-core/src/main/java/com/aol/micro/server/MicroserverApp.java
@@ -12,7 +12,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 
 import com.aol.micro.server.config.Config;
-import com.aol.micro.server.config.Configurer;
 import com.aol.micro.server.config.MicroserverConfigurer;
 import com.aol.micro.server.module.Module;
 import com.aol.micro.server.servers.ApplicationRegister;
@@ -24,89 +23,78 @@ import com.aol.simple.react.exceptions.ExceptionSoftener;
 import com.google.common.collect.Lists;
 
 /**
- * 
  * Startup class for Microserver instance
  * 
  * @author johnmcclean
- *
  */
 public class MicroserverApp {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private final List<Module> modules;
+
 	private final CompletableFuture end = new CompletableFuture();
-	private final ExceptionSoftener softener = ExceptionSoftener.singleton.factory
-			.getInstance();
+	private final ExceptionSoftener softener = ExceptionSoftener.singleton.factory.getInstance();
 
 	@Getter
 	private final ApplicationContext springContext;
 
 	/**
-	 * This will construct a Spring context for this Microserver instance.
-	 * The calling class will be used to determine the base package to auto-scan from for Spring Beans
-	 * It will attempt to pick up an @Microservice annotation first, if not present the package of the calling class
-	 * will be used.
+	 * This will construct a Spring context for this Microserver instance. The calling class will be used to determine the base package to auto-scan
+	 * from for Spring Beans It will attempt to pick up an @Microservice annotation first, if not present the package of the calling class will be
+	 * used.
 	 * 
-	 * @param modules Multiple Microservice end points that can be deployed within a single Spring context
+	 * @param modules
+	 *            Multiple Microservice end points that can be deployed within a single Spring context
 	 */
 	public MicroserverApp(Module... modules) {
 		this.modules = Lists.newArrayList(modules);
-		springContext = new SpringContextFactory(new MicroserverConfigurer().buildConfig(
-				extractClass()), extractClass(),
-				modules[0].getSpringConfigurationClasses())
-				.createSpringContext();
+		springContext = new SpringContextFactory(new MicroserverConfigurer().buildConfig(extractClass()), extractClass(),
+				modules[0].getSpringConfigurationClasses()).createSpringContext();
 
 	}
 
 	/**
-	 * This will construct a Spring context for this Microserver instance.
-	 * The provided class will be used to determine the base package to auto-scan from for Spring Beans
-	 * It will attempt to pick up an @Microservice annotation first, if not present the package of the provided class
-	 * will be used.
+	 * This will construct a Spring context for this Microserver instance. The provided class will be used to determine the base package to auto-scan
+	 * from for Spring Beans It will attempt to pick up an @Microservice annotation first, if not present the package of the provided class will be
+	 * used.
 	 * 
-	 * @param c Class used to configure Spring
-	 * @param modules Multiple Microservice end points that can be deployed within a single Spring context
+	 * @param c
+	 *            Class used to configure Spring
+	 * @param modules
+	 *            Multiple Microservice end points that can be deployed within a single Spring context
 	 */
 	public MicroserverApp(Class c, Module... modules) {
 
 		this.modules = Lists.newArrayList(modules);
-		springContext = new SpringContextFactory(
-				new MicroserverConfigurer().buildConfig(c), c,
-				modules[0].getSpringConfigurationClasses())
+		springContext = new SpringContextFactory(new MicroserverConfigurer().buildConfig(c), c, modules[0].getSpringConfigurationClasses())
 				.createSpringContext();
 
 	}
 
 	/**
-	 * This will construct a Spring context for this Microserver instance.
-	 * The provided Config object will be used to configure Spring
+	 * This will construct a Spring context for this Microserver instance. The provided Config object will be used to configure Spring
 	 * 
-	 * @param config Spring configuration
-	 * @param modules Multiple Microservice end points that can be deployed within a single Spring context
+	 * @param config
+	 *            Spring configuration
+	 * @param modules
+	 *            Multiple Microservice end points that can be deployed within a single Spring context
 	 */
 	public MicroserverApp(Config config, Module... modules) {
 
 		this.modules = Lists.newArrayList(modules);
 		config.set();
-		springContext = new SpringContextFactory(config,
-				modules[0].getSpringConfigurationClasses())
-				.createSpringContext();
+		springContext = new SpringContextFactory(config, modules[0].getSpringConfigurationClasses()).createSpringContext();
 
 	}
-	
-	
 
 	private Class extractClass() {
 		try {
-			return Class.forName(new Exception().getStackTrace()[2]
-					.getClassName());
+			return Class.forName(new Exception().getStackTrace()[2].getClassName());
 		} catch (ClassNotFoundException e) {
 			softener.throwSoftenedException(e);
 		}
 		return null; // unreachable normally
 	}
-
-	
 
 	public void stop() {
 
@@ -121,15 +109,12 @@ public class MicroserverApp {
 
 	public List<Thread> start() {
 
-		List<ServerApplication> apps = modules
-				.stream()
-				.map(module -> createServer(module)).collect(Collectors.toList());
+		List<ServerApplication> apps = modules.stream().map(module -> createServer(module)).collect(Collectors.toList());
 
 		ServerRunner runner;
 		try {
 
-			runner = new ServerRunner(
-					springContext.getBean(ApplicationRegister.class), apps, end);
+			runner = new ServerRunner(springContext.getBean(ApplicationRegister.class), apps, end);
 		} catch (BeansException e) {
 			runner = new ServerRunner(apps, end);
 		}
@@ -138,10 +123,9 @@ public class MicroserverApp {
 	}
 
 	private ServerApplication createServer(Module module) {
-		ServerApplication app = new GrizzlyApplicationFactory(springContext,
-				module).createApp();
-		
-		if(Config.instance().getSslProperties()!=null)
+		ServerApplication app = new GrizzlyApplicationFactory(springContext, module).createApp();
+
+		if (Config.instance().getSslProperties() != null)
 			return app.withSSLProperties(Config.instance().getSslProperties());
 		else
 			return app;
