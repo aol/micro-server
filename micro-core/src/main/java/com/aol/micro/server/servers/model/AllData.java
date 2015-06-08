20fdfc99cf6e4bf2bb2b7dc4a3f02ceb1f002713
Deegan, Edward <edward.deegan@teamaol.com>
2015-06-08 09:26:58 +0100

    add ServletRequestListener support

--- micro-core/src/main/java/com/aol/micro/server/servers/model/AllData.java
+++ micro-core/src/main/java/com/aol/micro/server/servers/model/AllData.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestListener;
 
 import lombok.Getter;
 import lombok.experimental.Builder;
@@ -19,12 +20,16 @@ public class AllData {
 	private final ImmutableList<FilterData> filterDataList;
 	private final ImmutableList<ServletData> servletDataList;
 	private final ImmutableList<ServletContextListener> servletContextListeners;
-	
+	private final ImmutableList<ServletRequestListener> servletRequestListeners;
+
 	public AllData(ServerData serverData, List<FilterData> filterDataList, 
 				List<ServletData> servletDataList,
-				List<ServletContextListener> servletContextListeners) {
+				List<ServletContextListener> servletContextListeners,
+				List<ServletRequestListener> servletRequestListeners  ) {
 
 		this.servletContextListeners = ImmutableList.copyOf(UsefulStaticMethods.either(servletContextListeners, new ArrayList<ServletContextListener>()));
+
+		this.servletRequestListeners = ImmutableList.copyOf(UsefulStaticMethods.either(servletRequestListeners, new ArrayList<ServletRequestListener>()));
 
 		this.filterDataList = ImmutableList.copyOf(UsefulStaticMethods.either(filterDataList, new ArrayList<FilterData>()));
 		this.servletDataList = ImmutableList.copyOf(UsefulStaticMethods.either(servletDataList, new ArrayList<ServletData>()));
