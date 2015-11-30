734c8bc0ec72ddbb954213fd08054ed3b3cc5db1
Nikita Sapozhnikov <nikita.sapozhnikov@teamaol.com>
2015-11-30 17:57:00 +0000

    Ability for micro-server to register hosts inside docker containers

--- micro-application-register/src/main/java/com/aol/micro/server/application/registry/ApplicationRegisterImpl.java
+++ micro-application-register/src/main/java/com/aol/micro/server/application/registry/ApplicationRegisterImpl.java
@@ -5,15 +5,16 @@ import java.net.UnknownHostException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import lombok.Getter;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.aol.cyclops.invokedynamic.ExceptionSoftener;
 import com.aol.micro.server.servers.ApplicationRegister;
 import com.aol.micro.server.servers.model.ServerData;
+
+import lombok.Getter;
 
 @Component
 public class ApplicationRegisterImpl implements ApplicationRegister {
@@ -21,6 +22,8 @@ public class ApplicationRegisterImpl implements ApplicationRegister {
 	@Getter
 	private volatile Application application;
 
+	@Value("${configuration.hostname:#null}")
+	private String customHostname;
 	
 	public ApplicationRegisterImpl() {
 		
@@ -30,7 +33,7 @@ public class ApplicationRegisterImpl implements ApplicationRegister {
 	public void register(ServerData[] data) {
 		String hostname;
 		try {
-			hostname = InetAddress.getLocalHost().getHostName();
+			hostname = customHostname == null ? InetAddress.getLocalHost().getHostName() : customHostname;
 			application = new Application(Stream
 					.of(data)
 					.map(next -> new RegisterEntry(next.getPort(), hostname, next.getModule().getContext(), next
