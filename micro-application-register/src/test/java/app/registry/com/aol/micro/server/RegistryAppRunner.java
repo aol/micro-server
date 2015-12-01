fcfde70d4ed5ae070f75c038e210225744d2448c
John McClean <john.mcclean@teamaol.com>
2015-12-01 11:05:04 +0000

    capture ip address for application register

--- micro-application-register/src/test/java/app/registry/com/aol/micro/server/RegistryAppRunner.java
+++ micro-application-register/src/test/java/app/registry/com/aol/micro/server/RegistryAppRunner.java
@@ -1,10 +1,10 @@
 package app.registry.com.aol.micro.server;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.io.IOException;
-import java.util.Properties;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.After;
@@ -12,8 +12,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.aol.micro.server.MicroserverApp;
+import com.aol.micro.server.application.registry.RegisterEntry;
 import com.aol.micro.server.config.Microserver;
-import com.aol.micro.server.spring.properties.PropertyFileConfig;
+import com.aol.micro.server.rest.client.nio.AsyncRestClient;
+import com.aol.micro.server.rest.jackson.JacksonUtil;
 import com.aol.micro.server.testing.RestAgent;
 
 @Microserver(properties={"service.registry.url","http://localhost:8080/registry-app"})
@@ -21,7 +23,7 @@ public class RegistryAppRunner {
 
 
 	RestAgent rest = new RestAgent();
-	
+	private final AsyncRestClient restAsync = new AsyncRestClient(100,2000);
 	MicroserverApp server;
 	@Before
 	public void startServer(){
@@ -44,9 +46,24 @@ public class RegistryAppRunner {
 		assertThat(rest.post("http://localhost:8080/registry-app/service-registry/schedule"),is("{\"status\":\"success\"}"));
 		Thread.sleep(1000);
 		assertThat(rest.getJson("http://localhost:8080/registry-app/service-registry/list"),containsString("[{\"port\":8080,"));
+		
+		sendPing(new RegisterEntry(8081,"use-ip","hello","world", new Date()));
+		Thread.sleep(1000);
+		System.out.println(rest.getJson("http://localhost:8080/registry-app/service-registry/list"));
+		assertThat(rest.getJson("http://localhost:8080/registry-app/service-registry/list"),containsString("[{\"port\":8081,"));
+		
 	
 	}
 	
+	private void sendPing(RegisterEntry entry) {
+		
+		try {
+
+			restAsync.post("http://localhost:8080/registry-app/service-registry/register", JacksonUtil.serializeToJson(entry)).join();
+		} catch (Exception e) {
+
+		}
+	}
 	
 	
 	
