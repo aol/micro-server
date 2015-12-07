b305fbc6b06acd13b9dc05206c4f1dc5d2a7fe00
John McClean <john.mcclean@teamaol.com>
2015-12-07 17:00:54 +0000

    working build

--- micro-general-exception-mapper/src/test/java/com/aol/micro/server/general/exception/mapper/GeneralExceptionMapperTest.java
+++ micro-general-exception-mapper/src/test/java/com/aol/micro/server/general/exception/mapper/GeneralExceptionMapperTest.java
@@ -1,11 +1,7 @@
 package com.aol.micro.server.general.exception.mapper;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -13,25 +9,19 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.io.EOFException;
-import java.util.Arrays;
 
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.NotAcceptableException;
-import javax.ws.rs.NotAllowedException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.NotSupportedException;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
 
 import app.custom.com.aol.micro.server.copy.MyException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
-import com.sun.xml.internal.ws.server.UnsupportedMediaException;
 
 public class GeneralExceptionMapperTest {
 
@@ -76,7 +66,7 @@ public class GeneralExceptionMapperTest {
 	@Test
 	public void whenJacksonException_thenNotInternalServerError_NoErrorsLogged() {
 		mapper=  new GeneralExceptionMapper(mockLogger);
-		assertThat(mapper.toResponse(new MyException()).getStatus(), is((Status.INTERNAL_SERVER_ERROR.getStatusCode())));
+		assertThat(mapper.toResponse(new MyLocalException()).getStatus(), is((Status.INTERNAL_SERVER_ERROR.getStatusCode())));
 		verify(mockLogger, times(0)).error(any(String.class), any(Object[].class));
 		verify(mockLogger, times(0)).error(any(String.class), any(Throwable.class), any(Object[].class));
 	}
