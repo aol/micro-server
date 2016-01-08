c8d8c1ce87245167bc8ad21f5be12e630a5a6de2
Ke Wang <ke.wang@teamaol.com>
2016-01-08 13:56:50 +0000

    Fix for S3Configuration

--- micro-s3/src/main/java/com/aol/micro/server/s3/S3Configuration.java
+++ micro-s3/src/main/java/com/aol/micro/server/s3/S3Configuration.java
@@ -1,28 +1,24 @@
 package com.aol.micro.server.s3;
 
+import lombok.Getter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-
+@Component
+@Getter
 public class S3Configuration {
+	
+	private final String accessKey;
+	private final String secretKey;
+	private final String sessionToken;
 
-	private AWSCredentials credentials;
-
+	@Autowired
 	public S3Configuration(@Value("${s3.accessKey}") String accessKey, @Value("${s3.secretKey}") String secretKey, 
 			@Value("s3.sessionToken:#null") String sessionToken) {
-		if (sessionToken == null) {
-			credentials = new BasicAWSCredentials(accessKey, secretKey);
-		} else {
-			credentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
-		}
-	}
-
-	@Bean
-	public AmazonS3Client getClient() {
-		return new AmazonS3Client(credentials);
-	}
+		this.accessKey = accessKey;
+		this.secretKey = secretKey;
+		this.sessionToken = sessionToken;
+	}	
 }
