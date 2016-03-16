2085ef9cdcc69c440675f2dff3e0ed423665ebbd
Ke Wang <ke.wang@teamaol.com>
2016-03-16 12:10:31 +0000

    Provide TransferManager from S3 plugin

--- micro-s3/src/main/java/com/aol/micro/server/s3/S3Configuration.java
+++ micro-s3/src/main/java/com/aol/micro/server/s3/S3Configuration.java
@@ -9,18 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class S3Configuration {
-	
+
 	private final String accessKey;
 	private final String secretKey;
 	private final String sessionToken;
 	private final String region;
+	private final int uploadThreads;
+	private final String uploadThreadNamePrefix;
 
 	@Autowired
-	public S3Configuration(@Value("${s3.accessKey}") String accessKey, @Value("${s3.secretKey}") String secretKey, 
-			@Value("${s3.sessionToken:#{null}}") String sessionToken, @Value("${s3.region:#{null}}") String region) {
+	public S3Configuration(@Value("${s3.accessKey}") String accessKey, @Value("${s3.secretKey}") String secretKey,
+			@Value("${s3.sessionToken:#{null}}") String sessionToken, @Value("${s3.region:#{null}}") String region,
+			@Value("${s3.upload.threads:5}") int uploadThreads,
+			@Value("${s3.upload.thread.name.prefix:s3-transfer-manager-worker-}") String uploadThreadNamePrefix) {
 		this.accessKey = accessKey;
 		this.secretKey = secretKey;
 		this.sessionToken = sessionToken;
 		this.region = region;
-	}	
+		this.uploadThreads = uploadThreads;
+		this.uploadThreadNamePrefix = uploadThreadNamePrefix;
+	}
 }
