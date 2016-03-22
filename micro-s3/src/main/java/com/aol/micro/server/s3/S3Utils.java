67e01c47900b4c9e10d56efa5ff04b6a1eb533c4
Nikita Sapozhnikov <nikita.sapozhnikov@teamaol.com>
2016-03-22 18:10:12 +0000

    Method for deleting objects in batches

--- micro-s3/src/main/java/com/aol/micro/server/s3/S3Utils.java
+++ micro-s3/src/main/java/com/aol/micro/server/s3/S3Utils.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest.KeyVersion;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.aol.cyclops.control.ReactiveSeq;
 
 @Component
 public class S3Utils {
@@ -35,11 +38,20 @@ public class S3Utils {
 		} while (listing.isTruncated());
 		return result;
 	}
+
 	/*
 	 * TODO implement smarter mechanism to reduce number of queries
 	 */
 	public <T> Stream<T> getSummariesStream(ListObjectsRequest req, Function<S3ObjectSummary, T> processor) {
 		return getAllSummaries(req).stream().map(processor);
+	}
+
+	public void delete(String bucketName, List<KeyVersion> objects) {
+		ReactiveSeq.fromList(objects).grouped(1000).forEach(l -> {
+			DeleteObjectsRequest req = new DeleteObjectsRequest(bucketName);
+			req.setKeys(l.toList());
+			client.deleteObjects(req);
+		});
 	}
 
 }
