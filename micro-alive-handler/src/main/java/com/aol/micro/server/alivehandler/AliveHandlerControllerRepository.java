61125a3e244c34f700045d9295976b2ed2bb6572
John McClean <john.mcclean@teamaol.com>
2016-06-08 14:40:50 +0100

    remove micro-alive-handler-dirs

--- micro-alive-handler/src/main/java/com/aol/micro/server/alivehandler/AliveHandlerControllerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aol.micro.server.alivehandler;
-
-import java.util.Optional;
-
-public interface AliveHandlerControllerRepository {
-	Optional<AliveHandlerController> get(String name);
-}
