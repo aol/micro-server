5932dc66a092dd7275fcecc87dab363749f32f62
Ke Wang <ke.wang@teamaol.com>
2016-06-24 16:59:41 +0100

    Fix micro-curator unit tests

---
+++ micro-curator/src/test/java/app/prototype/com/aol/micro/server/DummyLock.java
@@ -0,0 +1,20 @@
+package app.prototype.com.aol.micro.server;
+
+import org.springframework.stereotype.Component;
+
+import com.aol.micro.server.dist.lock.DistributedLockService;
+
+@Component
+public class DummyLock implements DistributedLockService {
+
+	@Override
+	public boolean tryLock(String key) {
+		return false;
+	}
+
+	@Override
+	public boolean tryReleaseLock(String key) {
+		return false;
+	}
+
+}
