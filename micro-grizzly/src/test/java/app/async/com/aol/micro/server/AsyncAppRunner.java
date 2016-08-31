e917f81819fdf729759631f20d1f489d3ad65685
John McClean <john.mcclean@teamaol.com>
2016-08-31 10:49:24 +0100

    fix for #249

--- micro-grizzly/src/test/java/app/async/com/aol/micro/server/AsyncAppRunner.java
+++ micro-grizzly/src/test/java/app/async/com/aol/micro/server/AsyncAppRunner.java
@@ -19,39 +19,40 @@ import com.aol.micro.server.testing.RestAgent;
 @Microserver
 public class AsyncAppRunner {
 
+    RestAgent rest = new RestAgent();
 
-	RestAgent rest = new RestAgent();
-	
-	MicroserverApp server;
-	@Before
-	public void startServer(){
-		
-		server = new MicroserverApp( ()-> "async-app");
-		server.start();
+    MicroserverApp server;
 
-	}
-	
-	@After
-	public void stopServer(){
-		server.stop();
-	}
-	
-	@Test
-	public void runAppAndBasicTest() throws InterruptedException, ExecutionException{
-		
-		Thread.sleep(2000);
-		
-		assertThat(rest.get("http://localhost:8080/async-app/async/expensive"),is(";test!;test!;test!"));
-	
-	}
-	
-	@Test
-	public void loadProperties() throws IOException{
-		
-		 Properties props = new PropertyFileConfig(true).propertyFactory() ;
-		assertThat(props.getProperty("test"),is("hello world"));
-	}
-	
-	
-	
+    @Before
+    public void startServer() {
+
+        server = new MicroserverApp(
+                                    () -> "async-app");
+        server.start();
+
+    }
+
+    @After
+    public void stopServer() {
+        server.stop();
+    }
+
+    @Test
+    public void runAppAndBasicTest() throws InterruptedException, ExecutionException {
+
+        Thread.sleep(2000);
+
+        assertThat(rest.get("http://localhost:8080/async-app/async/expensive"), is(";test!;test!;test!"));
+
+    }
+
+    @Test
+    public void loadProperties() throws IOException {
+
+        Properties props = new PropertyFileConfig(
+                                                  true).propertyFactory();
+        assertThat(props.getProperty("test"), is("hello world"));
+        assertThat(props.getProperty("override"), is("boo!"));
+    }
+
 }
