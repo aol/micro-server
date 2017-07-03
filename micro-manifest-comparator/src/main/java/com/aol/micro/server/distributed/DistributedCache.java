27f84fca2234570963d7316756995e95acd474a7
Gordon Morrow <gordon.morrow@teamaol.com>
2017-07-03 16:55:15 +0100

    Updating to DistributedCache to extend DistributedMap

--- micro-manifest-comparator/src/main/java/com/aol/micro/server/distributed/DistributedCache.java
+++ micro-manifest-comparator/src/main/java/com/aol/micro/server/distributed/DistributedCache.java
@@ -3,10 +3,7 @@ package com.aol.micro.server.distributed;
 
 import java.util.Optional;
 
-public interface DistributedCache<V> {
-    boolean put(String key, V value);
-    Optional<V> get(String key);
-    void delete(String key);
+public interface DistributedCache<V> extends DistributedMap<V> {
     boolean put(String key, int expiry, V value);
     boolean isAvailable();
     void setConnectionTested(boolean result);
--- micro-manifest-comparator/src/main/java/com/aol/micro/server/distributed/DistributedMap.java
+++ micro-manifest-comparator/src/main/java/com/aol/micro/server/distributed/DistributedMap.java
@@ -6,9 +6,4 @@ public interface DistributedMap<V> {
     boolean put(String key, V value);
     Optional<V> get(String key);
     void delete(String key);
-    default boolean put(String key, int expiry, V value){
-        return false;
-    }
-    default boolean isAvailable() { return false; }
-    default void setConnectionTested(boolean result){};
 }
