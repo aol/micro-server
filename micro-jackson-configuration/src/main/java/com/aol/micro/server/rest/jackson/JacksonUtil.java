c323bae44d414cac635e09db9d1ed51e850cec27
davidartplus <davidartplus@gmail.com>
2017-09-04 14:58:17 +0100

    Update JacksonUtil.java

--- micro-jackson-configuration/src/main/java/com/aol/micro/server/rest/jackson/JacksonUtil.java
+++ micro-jackson-configuration/src/main/java/com/aol/micro/server/rest/jackson/JacksonUtil.java
@@ -15,6 +15,7 @@ import com.aol.micro.server.jackson.JacksonMapperConfigurator;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 public final class JacksonUtil {
 
