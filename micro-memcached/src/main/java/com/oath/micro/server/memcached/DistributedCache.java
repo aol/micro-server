abb9081e0aafe8e06351fe63bbd1cf403915fd31
Arun Balakrishnan <arun.balakrishnan@verizonmedia.com>
2020-09-08 12:09:44 +0100

    add few more methods to micro-memcache

--- micro-memcached/src/main/java/com/oath/micro/server/memcached/DistributedCache.java
+++ micro-memcached/src/main/java/com/oath/micro/server/memcached/DistributedCache.java
@@ -1,9 +1,15 @@
 package com.oath.micro.server.memcached;
+
+import java.net.SocketAddress;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.Future;
 
 public interface DistributedCache<K, V> {
         void setConnectionTested(boolean result);
         boolean isAvailable();
         boolean add(K key, int exp, V value);
         Optional<V> get(K key);
+        Future<Boolean> flush();
+        Map<SocketAddress, Map<String, String>> getStats();
 }
--- micro-memcached/src/test/java/com/oath/micros/server/elasticache/MemcachedCacheImplTest.java
+++ micro-memcached/src/test/java/com/oath/micros/server/elasticache/MemcachedCacheImplTest.java
@@ -1,18 +1,29 @@
 package com.oath.micros.server.elasticache;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.stub;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 import com.oath.micro.server.memcached.MemcachedCacheImpl;
 import net.spy.memcached.internal.OperationFuture;
 import org.junit.Before;
 import org.junit.Test;
 import net.spy.memcached.MemcachedClient;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 public class MemcachedCacheImplTest {
 
     MemcachedClient memcachedClient;
+    OperationFuture<Boolean> mockedFuture;
 
     @Before
     public void setup() {
@@ -20,8 +31,19 @@ public class MemcachedCacheImplTest {
 
         stub(memcachedClient.get("key1")).toReturn("value1");
         stub(memcachedClient.get("key2")).toReturn("value2");
-        OperationFuture<Boolean> mockedFuture = mock(OperationFuture.class);
+        stub(memcachedClient.getStats()).toReturn(getStatsMap());
+        mockedFuture = mock(OperationFuture.class);
         stub(memcachedClient.add("keyAdd", 3600, "valueadd")).toReturn(mockedFuture);
+        stub(memcachedClient.flush()).toReturn(mockedFuture);
+    }
+
+    Map<SocketAddress, Map<String, String>> getStatsMap(){
+        Map<String, String> statsMap = new HashMap<>();
+        Map<SocketAddress, Map<String, String>> statsMapForAllHost = new HashMap<>();
+        statsMap.put("hitCount", "10");
+        statsMap.put("missCount", "1");
+        statsMapForAllHost.put(new InetSocketAddress(1234), statsMap);
+        return statsMapForAllHost;
     }
 
     @Test
@@ -38,25 +60,50 @@ public class MemcachedCacheImplTest {
     }
 
     @Test
-    public void notExistingKeyPutTest() {
+    public void notExistingKeyPutTest() throws ExecutionException, InterruptedException {
         MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 1);
-        assertEquals(false, cache.add("keyAdd", 3600, "valueadd"));
+        when(mockedFuture.get()).thenReturn(false);
+        assertFalse(cache.add("keyAdd", 3600, "valueadd"));
+    }
+
+    @Test
+    public void existingKeyPutTest() throws ExecutionException, InterruptedException {
+        MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 1);
+        when(mockedFuture.get()).thenReturn(true);
+        assertTrue(cache.add("keyAdd", 3600, "valueadd"));
+    }
+
+    @Test
+    public void existingKeyPutTestWithRetry() throws ExecutionException, InterruptedException {
+        MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 2);
+        when(mockedFuture.get()).thenReturn(false, true);
+        assertTrue(cache.add("keyAdd", 3600, "valueadd"));
     }
 
     @Test
     public void testIsAvailableFalse() {
         MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 1);
         cache.setConnectionTested(false);
-        assertEquals(false, cache.isAvailable());
+        assertFalse(cache.isAvailable());
     }
 
     @Test
     public void testIsAvailableTrue() {
         MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 1);
         cache.setConnectionTested(true);
-        assertEquals(true, cache.isAvailable());
+        assertTrue(cache.isAvailable());
     }
 
+    @Test
+    public void testFlush() throws ExecutionException, InterruptedException {
+        MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 1);
+        when(mockedFuture.get()).thenReturn(false);
+        assertFalse((boolean)cache.flush().get());
+    }
 
-
+    @Test
+    public void testGetStats() {
+        MemcachedCacheImpl cache = new MemcachedCacheImpl(memcachedClient, 3, 1);
+        assertEquals(cache.getStats().toString(), "{0.0.0.0/0.0.0.0:1234={hitCount=10, missCount=1}}");
+    }
 }
